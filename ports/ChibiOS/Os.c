1b7f0d8f15a24aa949a64f2efe79d5fa5d4bb225
Zoltan Molnar <molnarxzoli@gmail.com>
2020-01-03 18:51:47 +0100

    Add OS interface implementation for ChibiOS.

--- ports/ChibiOS/Os.c
+++ ports/ChibiOS/Os.c
@@ -7,6 +7,7 @@
 /* INCLUDES                                                                  */
 /*****************************************************************************/
 #include "Interface/Os.h"
+#include "ch.h"
 
 /*****************************************************************************/
 /* DEFINED CONSTANTS                                                         */
@@ -23,6 +24,10 @@
 /*****************************************************************************/
 /* DEFINITION OF GLOBAL CONSTANTS AND VARIABLES                              */
 /*****************************************************************************/
+static mutex_t modemLock;
+static mutex_t parserLock;
+static mutex_t bufferLock;
+static thread_reference_t writer;
 
 /*****************************************************************************/
 /* DECLARATION OF LOCAL FUNCTIONS                                            */
@@ -37,47 +42,56 @@
 /*****************************************************************************/
 void OS_Init(void)
 {
-
+  chMtxObjectInit(&modemLock);
+  chMtxObjectInit(&parserLock);
+  chMtxObjectInit(&bufferLock);
+  writer = NULL;
 }
 
 void OS_LockModem(void)
 {
-    
+  chMtxLock(&modemLock);
 }
 
 void OS_UnlockModem(void)
 {
-    
+  chMtxUnlock(&modemLock);
 }
 
 void OS_LockParser(void)
 {
-    
+  chMtxLock(&parserLock);
 }
 
 void OS_UnlockParser(void)
 {
-    
+  chMtxUnlock(&parserLock);
 }
 
 void OS_LockBuffer(void)
 {
-    
+  chMtxLock(&bufferLock);
 }
 
 void OS_UnlockBuffer(void)
 {
-    
+  chMtxUnlock(&bufferLock);
 }
 
 OS_Error_t OS_WaitForResponseWithTimeout(uint32_t timeoutInMs)
 {
-    
+  chSysLock();
+  msg_t msg    = chThdSuspendTimeoutS(&writer, chTimeMS2I(timeoutInMs));
+  writer = NULL;
+  chSysUnlock();
+
+  return (MSG_TIMEOUT == msg) ? OS_TIMEOUT : OS_NO_ERROR;
 }
 
 void OS_WakeUpThreadWaitingForResponse(void)
 {
-    
+  if (writer)
+    chThdResume(&writer, MSG_OK);
 }
 
 /****************************** END OF FILE **********************************/
